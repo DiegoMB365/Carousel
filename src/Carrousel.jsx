0ebe256588038b5422833c0438ed0875d65b639b
Diego Simonelli <diegomasterbets365@gmail.com>
2024-01-24 10:00:43 -0300

    fondo flechas

--- src/Carrousel.jsx
+++ src/Carrousel.jsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import Carousel from 'react-bootstrap/Carousel';
+import Carousel from "react-bootstrap/Carousel";
 import cat from "./assets/cat.jpg";
 import cat2 from "./assets/cat2.png";
 import music from "./assets/music.jpg";
@@ -10,88 +9,74 @@ import flecha from "./assets/flecha.png";
 
 export default function Carrousel() {
   const imageStyle = {
-    margin: '40px',
-    width: '300px',
-    height: '300px',
+    margin: "40px",
+    width: "300px",
+    height: "300px",
   };
 
   const textOverlayStyle = {
-    position: 'absolute',
-    left: '200px',
-    color: 'white',
-    fontSize: '24px',
-    fontWeight: 'bold',
-    margin: '5px',
+    position: "absolute",
+    left: "200px",
+    color: "white",
+    fontSize: "24px",
+    fontWeight: "bold",
+    margin: "5px",
     zIndex: 1,
+  };
+
+  const underlineStyle = {
+    borderBottom: "2px solid yellow",
   };
 
   const carouselStyle = {
-    position: 'relative',
-    backgroundColor: 'black',
+    position: "relative",
+    backgroundColor: "black",
+    width: "1140px",
+    height: "419px",
   };
 
   const arrowStyle = {
-    position: 'absolute',
-    height:"40px",
+    position: "absolute",
+    height: "40px",
     zIndex: 1,
-    right: "250px", 
-    center: '10px',
-    transform: 'rotate(90deg)', 
-    cursor: 'pointer', 
+    top: "4px",
+    left: "1000px",
+    transform: "rotate(90deg)",
+    cursor: "pointer",
   };
 
-
-const arrowStyle2 = {
-  position: 'absolute',
-  height: '40px',
-  zIndex: 1,
-  right: '200px', 
-  buttom: '10px', 
-  transform: 'rotate(-90deg)', 
-  cursor: 'pointer', 
-};
-
-
+  const arrowStyle2 = {
+    position: "absolute",
+    height: "40px",
+    zIndex: 1,
+    top: "4px",
+    left: "90px",
+    transform: "rotate(-90deg)",
+    cursor: "pointer",
+  };
 
   return (
     <>
-      <h2 style={textOverlayStyle}>TOP CASINO</h2>
-      <img style={arrowStyle} alt='Flecha izquierda' src={flecha} />
-      <img style={arrowStyle2} alt='Flecha derecha' src={flecha} />
-      <Carousel style={carouselStyle}>
+      <h2 style={textOverlayStyle}>
+        <span style={underlineStyle}>TOP</span> CASINO
+      </h2>
+      <Carousel
+        data-bs-theme='dark'
+        style={carouselStyle}
+        nextIcon={<img style={arrowStyle2} alt='Flecha derecha' src={flecha} />}
+        prevIcon={
+          <img style={arrowStyle} alt='Flecha izquierda' src={flecha} />
+        }
+      >
         <Carousel.Item>
-          <img
-            style={imageStyle}
-            src={cat}
-            alt="Primera foto"
-          />
-          <img
-            style={imageStyle}
-            src={music}
-            alt="Segunda foto"
-          />
-          <img
-            style={imageStyle}
-            src={cat2}
-            alt="Tercera foto"
-          />
+          <img style={imageStyle} src={cat} alt='Primera foto' />
+          <img style={imageStyle} src={music} alt='Segunda foto' />
+          <img style={imageStyle} src={cat2} alt='Tercera foto' />
         </Carousel.Item>
         <Carousel.Item>
-          <img
-            style={imageStyle}
-            src={love}
-            alt="Primera foto"
-          />
-          <img
-            style={imageStyle}
-            src={butterfly}
-            alt="Segunda foto"
-          />
-          <img
-            style={imageStyle}
-            src={music2}
-            alt="Tercera foto"
-          />
+          <img style={imageStyle} src={love} alt='Primera foto' />
+          <img style={imageStyle} src={butterfly} alt='Segunda foto' />
+          <img style={imageStyle} src={music2} alt='Tercera foto' />
         </Carousel.Item>
       </Carousel>
     </>
